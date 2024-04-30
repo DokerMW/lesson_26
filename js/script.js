5d65a35ad4cb9326a70f89ee5484c271b6101d9b
DokerMW <abddoker@gmail.com>
2024-04-30 22:39:35 +0500

    add: complete lesson_26

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const getData = (url) => 
+	fetch(url)
+	.then(response => response.json())
+
+const sendData = (url, data) => 
+	fetch(url, {
+		method: 'POST',
+		body: data,
+		headers: {
+			'Content-type': 'application/json; charset=UTF-8',
+		},
+	})
+	.then(response => response.json())
+
+getData('./db.json')
+	.then(data => 
+		sendData('https://jsonplaceholder.typicode.com/posts', JSON.stringify(data))
+			.then(data => console.log(data))
+			.catch(error => console.log(error))
+	)
+	.catch(error => console.log(error))
+	
